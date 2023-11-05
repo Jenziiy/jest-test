d4538ba454f5950c62d5c1f2be243fbae3e6c945
user.email <developerjenzii@gmail.com>
2023-11-06 00:30:39 +0100

    add 3d test jest docs

--- sum.test.js
+++ sum.test.js
@@ -2,4 +2,16 @@ const mod = require('./sum.js')
 
 test('adds 1+2 to eq 3', () => {
   expect(mod.sum(1,2)).toBe(3);
-})
+});
+
+test('object assignment',() =>{
+  const data = {one: 1};
+  data['two'] = 2;
+  expect(data).toEqual({one: 1, two: 2});
+});
+
+test('adding positive numbers is not zero', () => {
+  for (let i = 0; i < 10; i++) {
+    for (let j = 1; j < 10; j++) {
+      expect(a+b).not.toBe(0);
+    }}});
